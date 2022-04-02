69abc90535661b54cb3364ccfe512fb5f487817a
Abdulrasheed <55736584+AbdulrasheedAdediran@users.noreply.github.com>
2022-04-02 15:33:26 +0100

    chain id swap

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { formatDate } from "./utils/helpers";
 import Footer from "./components/Footer/Footer";
 import { ethers, utils, Contract } from "ethers";
 import BRTTokenAbi from "./utils/web3/abi.json";
-const BRTTokenAddress = "0x12cCa91846018901D3A5367b4b7d2c641D9Ab1B7";
+const BRTTokenAddress = "0x29B41DA9E429B7E9124493E114dbF921089A4c11";
 
 function App() {
   // a flag for keeping track of whether or not a user is connected
@@ -66,7 +66,7 @@ function App() {
       const networkId = await window.ethereum.request({
         method: "eth_chainId",
       });
-      if (Number(networkId) !== 80001) return;
+      if (Number(networkId) !== 4) return;
       const accountDetails = await getAccountDetails(accounts[0]);
 
       setUserInfo({
@@ -87,7 +87,7 @@ function App() {
 
   // handler for handling chain/network changed
   const handleChainChanged = async (chainid) => {
-    if (Number(chainid) !== 80001) {
+    if (Number(chainid) !== 4) {
       setConnected(false);
       setUserInfo({
         matic_balance: 0,
@@ -115,7 +115,7 @@ function App() {
   // an handler to eagerly connect user and fetch their data
   const eagerConnect = async () => {
     const networkId = await window.ethereum.request({ method: "eth_chainId" });
-    if (Number(networkId) !== 80001) return;
+    if (Number(networkId) !== 4) return;
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const accounts = await provider.listAccounts();
     if (!accounts.length) return;
