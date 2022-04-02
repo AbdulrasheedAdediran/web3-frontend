dcee28d05f79ff8c269aff8844f538e84520d2cd
Abdulrasheed <55736584+AbdulrasheedAdediran@users.noreply.github.com>
2022-04-02 16:36:40 +0100

    final

--- src/components/MyStake/MyStake.js
+++ src/components/MyStake/MyStake.js
@@ -65,7 +65,7 @@ const MyStake = ({
               })}
               disabled={!connected}
             >
-              Unstake
+              Withdraw
             </button>
           </form>
         </div>
